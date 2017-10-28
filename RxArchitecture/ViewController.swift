34e380886195ddd2f0c180e69ffff215e5f9a7b1
JimCampagno <jimcampagno@gmail.com>
2017-10-28 14:23:36 -0400

    Add initial project

---
+++ RxArchitecture/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  RxArchitecture
+//
+//  Created by Jim Campagno on 10/28/17.
+//  Copyright © 2017 Jim Campagno. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
